ec2483ca72dcdb60947003c3498eb9fe9ad2033b
malikibrahim10203 <malikibr4h1@gmail.com>
2024-01-15 11:17:39 +0700

    inisiasi awal

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loginstyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card">
+        <div class="title">
+            <h2>Login Account</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div class="mb-3 row text-center">
+                <label for="username" class="col-sm-2 col-form-label"><i class="bi bi-person-fill" style="font-size: 20px; color: #6B728E;"></i></label>
+                <div class="col-sm-10">
+                    <input type="text" class="input" name="username" id="username" placeholder="username">
+                </div>
+            </div>
+            <div class="mb-3 row text-center">
+                <label for="password" class="col-sm-2 col-form-label"><i class="bi bi-lock " style="font-size: 20px; color: #6B728E;"></i></i></label>
+                <div class="col-sm-10">
+                    <input type="text" class="input" name="password" id="password" placeholder="password">
+                </div>
+            </div>
+            <input type="button" value="Login" class="btn-kirim">
+        </div>
+    </div>
+</body>
+</html>
